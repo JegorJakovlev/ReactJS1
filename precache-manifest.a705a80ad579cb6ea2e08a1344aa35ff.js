2ce1c507c2ae42bc1682dc77b956489e09249154
IljaAstasev <Ilja.astasev@ivkhk.ee>
2023-11-08 12:56:46 +0200

    Updates

---
+++ precache-manifest.a705a80ad579cb6ea2e08a1344aa35ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "305623557d99770bdda6",
+    "url": "/ReactJS1/static/css/main.f3e1568f.chunk.css"
+  },
+  {
+    "revision": "305623557d99770bdda6",
+    "url": "/ReactJS1/static/js/main.30562355.chunk.js"
+  },
+  {
+    "revision": "038165661100e9f920c2",
+    "url": "/ReactJS1/static/css/1.26b01676.chunk.css"
+  },
+  {
+    "revision": "038165661100e9f920c2",
+    "url": "/ReactJS1/static/js/1.03816566.chunk.js"
+  },
+  {
+    "revision": "3b945ae2c56774c56aad",
+    "url": "/ReactJS1/static/js/runtime~main.3b945ae2.js"
+  },
+  {
+    "revision": "e1005717043501384d4fe40a32041f7b",
+    "url": "/ReactJS1/index.html"
+  }
+];
